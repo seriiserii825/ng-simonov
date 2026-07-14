bbabcbb23a1b6392909ec450776b547347c35867
serii burduja <seriiburduja@gmail.com>
2026-07-14 09:30:36 +0300

    feat: clear child component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ColorComponent } from './components/color-component/color-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ColorComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('ng-simonov');
-}
+export class App {}
