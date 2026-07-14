345c42aeb2745667e58408871ed9df8a39844a3d
serii burduja <seriiburduja@gmail.com>
2026-07-14 10:18:37 +0300

    feat: finished calculator

--- src/app/pages/calculator-page/calculator-page.ts
+++ src/app/pages/calculator-page/calculator-page.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator-page.html',
 })
-export class CalculatorPage {}
+export class CalculatorPage {
+  firstNumber: number = 1;
+  secondNumber: number = 1;
+  operations: string[] = ['+', '-', '*', '/'];
+  operation: string = '+';
+  result: number = 1;
+
+  calculate() {
+    switch (this.operation) {
+      case '+':
+        this.result = this.firstNumber + this.secondNumber;
+        break;
+      case '-':
+        this.result = this.firstNumber - this.secondNumber;
+        break;
+      case '*':
+        this.result = this.firstNumber * this.secondNumber;
+        break;
+      case '/':
+        if (this.secondNumber !== 0) {
+          this.result = this.firstNumber / this.secondNumber;
+        } else {
+          alert('Cannot divide by zero');
+          this.result = NaN; // Set result to NaN to indicate an error
+        }
+        break;
+      default:
+        alert('Invalid operation');
+        break;
+    }
+  }
+}
